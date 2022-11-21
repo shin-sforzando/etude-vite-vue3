b661c4881d6805fb653383eb8964d9faba3b4e4d
SHINICHIRO SUZUKI <shin@sforzando.co.jp>
2022-11-21 13:34:17 +0900

    feat: :+1: introduce Storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,6 +17,7 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-essential',
     'standard-with-typescript',
+    'plugin:storybook/recommended',
     '@vue/typescript/recommended',
     '@vue/prettier',
   ],
