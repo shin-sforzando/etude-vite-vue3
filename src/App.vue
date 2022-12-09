ac311283377888796788fa58955c949784c37629
SHINICHIRO SUZUKI <shin@sforzando.co.jp>
2022-12-10 00:39:18 +0900

    wip: :construction: try to implement using slot props

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,18 @@
 <script setup lang="ts">
-  import { provide, reactive } from 'vue'
-  import BaseSection from '@/components/BaseSection.vue'
-  import { Member } from '@/interfaces'
-
-  const memberList = new Map<number, Member>()
-  memberList.set(1, {
-    id: 1,
-    name: 'John',
-    email: 'john@example.com',
-    points: 10,
-    note: 'Special Guest',
-  })
-  memberList.set(2, {
-    id: 2,
-    name: 'Jane',
-    email: 'jane@example.com',
-    points: 20,
-  })
-  provide('memberList', reactive(memberList))
+  import OneSection from '@/components/OneSection.vue'
 </script>
 
 <template>
-  <BaseSection />
+  <section>
+    <OneSection>
+      <template v-slot="{ memberInfo }">
+        <dl>
+          <dt>Name</dt>
+          <dd>{{ memberInfo.name }}</dd>
+          <dt>Status</dt>
+          <dd>{{ memberInfo.status }}</dd>
+        </dl>
+      </template>
+    </OneSection>
+  </section>
 </template>
---
+++ src/components/OneSection.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import { reactive } from 'vue'
+
+  const memberInfo = reactive({
+    name: 'John',
+    status: '連絡がつきません',
+  })
+</script>
+
+<template>
+  <section>
+    <slot v-bind:memberInfo="memberInfo">
+      <h1>{{ memberInfo.name }}</h1>
+      <p>{{ memberInfo.status }}</p>
+    </slot>
+  </section>
+</template>
