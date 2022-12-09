767f0185524b07986bce14674502855605ac6f8e
SHINICHIRO SUZUKI <shin@sforzando.co.jp>
2022-12-09 20:03:08 +0900

    wip: :construction: chap03

---
+++ src/components/OneSection.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+  const props = defineProps<{
+    rand: number
+  }>()
+
+  const emits = defineEmits<{
+    (e: 'createNewRand'): void
+  }>()
+
+  const onNewRandButtonClick = (): void => {
+    emits('createNewRand')
+  }
+</script>
+
+<template>
+  <section class="box">
+    <p>rand in Children: {{ rand }}</p>
+    <button v-on:click="onNewRandButtonClick">New Random</button>
+  </section>
+</template>
+
+<style scoped>
+  .box {
+    border: green 1px solid;
+    margin: 10px;
+  }
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/etude-vite-vue3/',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': '/src',
     },
   },
-  plugins: [vue()],
 })
