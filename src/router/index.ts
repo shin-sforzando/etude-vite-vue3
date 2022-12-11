0b8dbb52c50f0de5f4a4cbe00d33e2b3dee1213c
SHINICHIRO SUZUKI <shin@sforzando.co.jp>
2022-12-11 20:19:50 +0900

    feat: :+1: introduce vue-router

---
+++ src/router/index.ts
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import AppTop from '@/views/AppTop.vue'
+
+const routeSettings: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'AppTop',
+    component: AppTop,
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: routeSettings,
+})
+
+export default router
---
+++ src/views/AppTop.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>TOP</h1>
+  <nav id="breadcrumbs">
+    <ul>
+      <li>TOP</li>
+    </ul>
+  </nav>
+  <section><p>会員管理</p></section>
+</template>
